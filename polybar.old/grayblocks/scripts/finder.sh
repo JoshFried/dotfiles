7bae7f924b807e5e0b5fb4997dddfbeea61f207d
Josh Fried <josh.fried13@gmail.com>
2021-03-13 16:02:32 -0500

    changed it all

--- polybar.old/grayblocks/scripts/finder.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-
-rofi -no-config -no-lazy-grab -show -theme ~/.config/polybar/grayblocks/scripts/rofi/launcher.rasi 
--- polybar/cuts/launch.sh
+++ polybar/cuts/launch.sh
@@ -10,6 +10,17 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch the bar
-polybar -q top -c "$DIR"/config.ini &
-polybar -q bottom -c "$DIR"/config.ini &
+Launch the bar
+# polybar -q top -c "$DIR"/config.ini &
+# polybar -q bottom -c "$DIR"/config.ini &
+# polybar -q second -c "$DIR"/config.ini &
+#
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload top -c "$DIR"/config.ini &
+    MONITOR=$m polybar --reload bottom -c "$DIR"/config.ini & 
+  done
+else
+  polybar --reload example &
+
+fi
