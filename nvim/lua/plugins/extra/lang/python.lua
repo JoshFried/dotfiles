306a62a2f53bace097c6077dc60076483d77dea6
Josh Fried <112121129+joshfried-aws@users.noreply.github.com>
2023-08-21 17:03:36 -0400

    python stuff

--- nvim/lua/plugins/extra/lang/python.lua
+++ nvim/lua/plugins/extra/lang/python.lua
@@ -1,72 +1,112 @@
+if not require("config").pde.python then
+    return {}
+end
+
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = function(_, opts)
-			vim.list_extend(opts.ensure_installed, { "python" })
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		opts = {
-			servers = {
-				pyright = {
-					settings = {
-						python = {
-							analysis = {
-								typeCheckingMode = "off",
-								autoSearchPaths = true,
-								useLibraryCodeForTypes = true,
-								diagnosticMode = "workspace",
-							},
-						},
-					},
-				},
-			},
-			setup = {
-				pyright = function(_, opts)
-					local lsp_utils = require("plugins.lsp.utils")
-					lsp_utils.on_attach(function(client, buffer)
-            -- stylua: ignore
-            if client.name == "pyright" then
-              vim.keymap.set("n", "<leader>tC", function() require("dap-python").test_class() end, { buffer = buffer, desc = "Debug Class" })
-              vim.keymap.set("n", "<leader>tM", function() require("dap-python").test_method() end, { buffer = buffer, desc = "Debug Method" })
-              vim.keymap.set("v", "<leader>tS", function() require("dap-python").debug_selection() end, { buffer = buffer, desc = "Debug Selection" })
-            end
-					end)
-				end,
-			},
-		},
-	},
-	{
-		"mfussenegger/nvim-dap",
-		dependencies = { "mfussenegger/nvim-dap-python" },
-		opts = {
-			setup = {
-				debugpy = function(_, _)
-					require("dap-python").setup("python", {})
-					table.insert(require("dap").configurations.python, {
-						type = "python",
-						request = "attach",
-						connect = {
-							port = 5678,
-							host = "127.0.0.1",
-						},
-						mode = "remote",
-						name = "container attach debug",
-						cwd = vim.fn.getcwd(),
-						pathmappings = {
-							{
-								localroot = function()
-									return vim.fn.input("local code folder > ", vim.fn.getcwd(), "file")
-								end,
-								remoteroot = function()
-									return vim.fn.input("container code folder > ", "/", "file")
-								end,
-							},
-						},
-					})
-				end,
-			},
-		},
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = function(_, opts)
+            vim.list_extend(opts.ensure_installed, { "ninja", "python", "rst", "toml" })
+        end,
+    },
+    {
+        "jose-elias-alvarez/null-ls.nvim",
+        opts = function(_, opts)
+            local nls = require "null-ls"
+            table.insert(opts.sources, nls.builtins.formatting.black)
+        end,
+    },
+    {
+        "williamboman/mason.nvim",
+        opts = function(_, opts)
+            vim.list_extend(opts.ensure_installed, { "debugpy", "black", "ruff" })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = {
+                ruff_lsp = {},
+                pyright = {
+                    settings = {
+                        python = {
+                            analysis = {
+                                autoImportCompletions = true,
+                                typeCheckingMode = "off",
+                                autoSearchPaths = true,
+                                useLibraryCodeForTypes = true,
+                                diagnosticMode = "openFilesOnly",
+                                stubPath = vim.fn.stdpath "data" .. "/lazy/python-type-stubs/stubs",
+                            },
+                        },
+                    },
+                },
+            },
+            setup = {
+                ruff_lsp = function()
+                    local lsp_utils = require "plugins.lsp.utils"
+                    lsp_utils.on_attach(function(client, _)
+                        if client.name == "ruff_lsp" then
+                            -- Disable hover in favor of Pyright
+                            client.server_capabilities.hoverProvider = false
+                        end
+                    end)
+                end,
+                pyright = function(_, _)
+                    local lsp_utils = require "plugins.lsp.utils"
+                    lsp_utils.on_attach(function(client, bufnr)
+                        local map = function(mode, lhs, rhs, desc)
+                            if desc then
+                                desc = desc
+                            end
+                            vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc, buffer = bufnr, noremap = true })
+                        end
+                        -- stylua: ignore
+                        if client.name == "pyright" then
+                            map("n", "<leader>lo", "<cmd>PyrightOrganizeImports<cr>", "Organize Imports")
+                            map("n", "<leader>lC", function() require("dap-python").test_class() end, "Debug Class")
+                            map("n", "<leader>lM", function() require("dap-python").test_method() end, "Debug Method")
+                            map("v", "<leader>lE", function() require("dap-python").debug_selection() end,
+                                "Debug Selection")
+                        end
+                    end)
+                end,
+            },
+        },
+    },
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "mfussenegger/nvim-dap-python",
+            config = function()
+                local path = require("mason-registry").get_package("debugpy"):get_install_path()
+                require("dap-python").setup(path .. "/venv/bin/python")
+            end,
+        },
+    },
+    {
+        "nvim-neotest/neotest",
+        dependencies = {
+            "nvim-neotest/neotest-python",
+        },
+        opts = function(_, opts)
+            vim.list_extend(opts.adapters, {
+                require "neotest-python" {
+                    dap = { justmycode = false },
+                    runner = "unittest",
+                },
+            })
+        end,
+    },
+    {
+        "microsoft/python-type-stubs",
+        cond = false,
+    },
+    {
+        "linux-cultist/venv-selector.nvim",
+        cmd = "VenvSelect",
+        opts = {},
+        keys = { { "<leader>lv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv" } },
+    },
 }
+
--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -1,156 +1,130 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		event = "BufReadPre",
-		dependencies = {
-			{ "folke/neoconf.nvim", cmd = "Neoconf", config = true },
-			{
-				"folke/neodev.nvim",
-				opts = {
-					library = { plugins = { "neotest", "nvim-dap-ui" }, types = true },
-				},
-			},
-			{ "j-hui/fidget.nvim", config = true },
-			{ "smjonas/inc-rename.nvim", config = true },
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-nvim-lsp-signature-help",
-			"jay-babu/mason-null-ls.nvim",
-		},
-		opts = {
-			servers = {
-				lua_ls = {
-					settings = {
-						Lua = {
-							workspace = {
-								checkThirdParty = false,
-							},
-							completion = { callSnippet = "Replace" },
-							telemetry = { enable = false },
-							hint = {
-								enable = false,
-							},
-						},
-					},
-				},
-				dockerls = {},
-				marksman = {},
-				yamlls = {
-					settings = {
-						yaml = {
-							keyOrdering = false,
-						},
-					},
-				},
-				lemminx = {},
-			},
-			setup = {
-				lua_ls = function(_, _)
-					local lsp_utils = require("plugins.lsp.utils")
-					lsp_utils.on_attach(function(client, buffer)
-						if client.name == "lua_ls" then
-							vim.keymap.set("n", "<leader>dX", function()
-								require("osv").run_this()
-							end, { buffer = buffer, desc = "OSV Run" })
-							vim.keymap.set("n", "<leader>dL", function()
-								require("osv").launch({ port = 8086 })
-							end, { buffer = buffer, desc = "OSV Launch" })
-						end
-					end)
-				end,
-			},
-		},
-		config = function(plugin, opts)
-			require("plugins.lsp.servers").setup(plugin, opts)
-		end,
-	},
-	{
-		"williamboman/mason.nvim",
-		cmd = "Mason",
-		keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
-		opts = {
-			ensure_installed = {
-				"stylua",
-				"ruff",
-				"codelldb",
-				"debugpy",
-			},
-		},
-		config = function(_, opts)
-			require("mason").setup()
-			local mr = require("mason-registry")
-			for _, tool in ipairs(opts.ensure_installed) do
-				local p = mr.get_package(tool)
-				if not p:is_installed() then
-					p:install()
-				end
-			end
-		end,
-	},
-	-- {
-	-- 	"jose-elias-alvarez/null-ls.nvim",
-	-- 	event = "BufReadPre",
-	-- 	dependencies = { "mason.nvim" },
-	-- 	config = function()
-	-- 		local nls = require("null-ls")
-	-- 		local fmt = nls.builtins.formatting
-	-- 		local root_pattern = require("lspconfig.util").root_pattern
-
-	-- 		local gotest = require("go.null_ls").gotest()
-	-- 		nls.setup({
-	-- 			sources = {
-	-- 				fmt.stylua,
-	-- 				fmt.black,
-	-- 				fmt.shfmt,
-	-- 				fmt.prettierd,
-	-- 				fmt.golines,
-	-- 				nls.builtins.diagnostics.eslint_d.with({
-	-- 					root_dir = root_pattern(".eslintrc.*", "node_modules", ".git"),
-	-- 					diagnostics_format = "[eslint_d] #{m}\n(#{c})",
-	-- 				}),
-	-- 				nls.builtins.diagnostics.ruff.with({ extra_args = { "--max-line-length=180" } }),
-	-- 			},
-	-- 		})
-	-- 	end,
-	-- },
-	{
-		"jose-elias-alvarez/null-ls.nvim",
-		event = "BufReadPre",
-		dependencies = { "mason.nvim" },
-		opts = function()
-			local nls = require("null-ls")
-			return {
-				root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
-				sources = {
-					nls.builtins.formatting.shfmt,
-				},
-			}
-		end,
-	},
-	{
-		"jay-babu/mason-null-ls.nvim",
-		opts = { ensure_installed = nil, automatic_installation = true, automatic_setup = false },
-	},
-	{ "numToStr/Comment.nvim", opts = {} },
-	{
-		"utilyre/barbecue.nvim",
-		event = "VeryLazy",
-		dependencies = {
-			"neovim/nvim-lspconfig",
-			"SmiteshP/nvim-navic",
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = true,
-		enable = false,
-	},
-	{
-		"folke/trouble.nvim",
-		cmd = { "TroubleToggle", "Trouble" },
-		opts = { use_diagnostic_signs = true },
-		keys = {
-			{ "<leader>cd", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics" },
-			{ "<leader>cD", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics" },
-		},
-	},
+    {
+        "neovim/nvim-lspconfig",
+        event = "BufReadPre",
+        dependencies = {
+            { "folke/neoconf.nvim",      cmd = "Neoconf", config = true },
+            {
+                "folke/neodev.nvim",
+                opts = {
+                    library = { plugins = { "neotest", "nvim-dap-ui" }, types = true },
+                },
+            },
+            { "j-hui/fidget.nvim",       config = true },
+            { "smjonas/inc-rename.nvim", config = true },
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-nvim-lsp-signature-help",
+            "jay-babu/mason-null-ls.nvim",
+        },
+        opts = {
+            servers = {
+                lua_ls = {
+                    settings = {
+                        Lua = {
+                            workspace = {
+                                checkThirdParty = false,
+                            },
+                            completion = { callSnippet = "Replace" },
+                            telemetry = { enable = false },
+                            hint = {
+                                enable = false,
+                            },
+                        },
+                    },
+                },
+                dockerls = {},
+                marksman = {},
+                yamlls = {
+                    settings = {
+                        yaml = {
+                            keyOrdering = false,
+                        },
+                    },
+                },
+                lemminx = {},
+            },
+            setup = {
+                lua_ls = function(_, _)
+                    local lsp_utils = require("plugins.lsp.utils")
+                    lsp_utils.on_attach(function(client, buffer)
+                        if client.name == "lua_ls" then
+                            vim.keymap.set("n", "<leader>dX", function()
+                                require("osv").run_this()
+                            end, { buffer = buffer, desc = "OSV Run" })
+                            vim.keymap.set("n", "<leader>dL", function()
+                                require("osv").launch({ port = 8086 })
+                            end, { buffer = buffer, desc = "OSV Launch" })
+                        end
+                    end)
+                end,
+            },
+        },
+        config = function(plugin, opts)
+            require("plugins.lsp.servers").setup(plugin, opts)
+        end,
+    },
+    {
+        "williamboman/mason.nvim",
+        cmd = "Mason",
+        keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+        opts = {
+            ensure_installed = {
+                "stylua",
+                "ruff",
+                "codelldb",
+                "debugpy",
+            },
+        },
+        config = function(_, opts)
+            require("mason").setup()
+            local mr = require("mason-registry")
+            for _, tool in ipairs(opts.ensure_installed) do
+                local p = mr.get_package(tool)
+                if not p:is_installed() then
+                    p:install()
+                end
+            end
+        end,
+    },
+    {
+        "jose-elias-alvarez/null-ls.nvim",
+        event = "BufReadPre",
+        dependencies = { "mason.nvim" },
+        opts = function()
+            local nls = require("null-ls")
+            return {
+                root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+                sources = {
+                    nls.builtins.formatting.shfmt,
+                },
+            }
+        end,
+    },
+    {
+        "jay-babu/mason-null-ls.nvim",
+        opts = { ensure_installed = nil, automatic_installation = true, automatic_setup = false },
+    },
+    { "numToStr/Comment.nvim", opts = {} },
+    {
+        "utilyre/barbecue.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            "neovim/nvim-lspconfig",
+            "SmiteshP/nvim-navic",
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = true,
+        enable = false,
+    },
+    {
+        "folke/trouble.nvim",
+        cmd = { "TroubleToggle", "Trouble" },
+        opts = { use_diagnostic_signs = true },
+        keys = {
+            { "<leader>cd", "<cmd>TroubleToggle document_diagnostics<cr>",  desc = "Document Diagnostics" },
+            { "<leader>cD", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics" },
+        },
+    },
 }
