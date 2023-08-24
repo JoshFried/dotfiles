f9526f0e045674a3fa9fbf433de1ed1ed98622fe
Josh Fried <112121129+joshfried-aws@users.noreply.github.com>
2023-08-24 19:30:37 -0400

    changing shiftwidth for ts/js files, modifying some golang stuff

--- nvim/lua/plugins/navbuddy.lua
+++ nvim/lua/plugins/navbuddy.lua
@@ -1,30 +1,32 @@
+local ignoredNavBuddyClients = {
+    ["eslint"] = true,
+    ["null-ls"] = true,
+    ["golangci_lint_ls"] = true,
+    ["tsserver"] = true
+}
+
 return {
-	{
-		"SmiteshP/nvim-navbuddy",
-		event = "VeryLazy",
-		dependencies = {
-			"neovim/nvim-lspconfig",
-			"SmiteshP/nvim-navic",
-			"MunifTanjim/nui.nvim",
-		},
-    --stylua: ignore
-    keys = {
-      { "<leader>vo", function() require("nvim-navbuddy").open() end, desc = "Code Outline (navbuddy)", },
+    {
+        "SmiteshP/nvim-navbuddy",
+        event = "VeryLazy",
+        dependencies = {
+            "neovim/nvim-lspconfig",
+            "SmiteshP/nvim-navic",
+            "MunifTanjim/nui.nvim",
+        },
+        --stylua: ignore
+        keys = {
+            { "<leader>vo", function() require("nvim-navbuddy").open() end, desc = "Code Outline (navbuddy)", },
+        },
+        opts = {},
+        config = function()
+            local lsp_utils = require("plugins.lsp.utils")
+            lsp_utils.on_attach(function(client, buffer)
+                if ignoredNavBuddyClients[client.name] == nil then
+                    local navbuddy = require("nvim-navbuddy")
+                    navbuddy.attach(client, buffer)
+                end
+            end)
+        end,
     },
-		opts = {},
-		config = function()
-			local lsp_utils = require("plugins.lsp.utils")
-			lsp_utils.on_attach(function(client, buffer)
-				if client.name ~= "null-ls" then
-					local navbuddy = require("nvim-navbuddy")
-
-					if client.name == "eslint" then
-						return
-					end
-
-					navbuddy.attach(client, buffer)
-				end
-			end)
-		end,
-	},
 }
