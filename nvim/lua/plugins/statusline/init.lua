5834a89f0a91e475331038fa4141d866e3c0da4d
Josh Fried <josh.fried13@gmail.com>
2024-02-08 21:19:49 -0500

    ts context

--- nvim/lua/plugins/statusline/init.lua
+++ nvim/lua/plugins/statusline/init.lua
@@ -1,54 +1,55 @@
 return {
-	{
-		"nvim-lualine/lualine.nvim",
-		event = "VeryLazy",
+    {
+        "nvim-lualine/lualine.nvim",
+        event = "VeryLazy",
 
-		config = function()
-			local components = require("plugins.statusline.components")
+        config = function()
+            local components = require("plugins.statusline.components")
 
-			require("lualine").setup({
-				options = {
-					icons_enabled = true,
-					theme = "auto",
-					component_separators = {},
-					section_separators = {},
-					disabled_filetypes = {
-						statusline = { "alpha", "lazy", "fugitive", "" },
-						winbar = {
-							"help",
-							"alpha",
-							"lazy",
-						},
-					},
-					always_divide_middle = true,
-					globalstatus = true,
-				},
-				sections = {
-					lualine_a = { "mode" },
-					lualine_b = { components.git_repo, "branch" },
+            require("lualine").setup({
+                options = {
+                    icons_enabled = true,
+                    theme = "auto",
+                    component_separators = {},
+                    section_separators = {},
+                    disabled_filetypes = {
+                        statusline = { "alpha", "lazy", "fugitive", "" },
+                        winbar = {
+                            "help",
+                            "alpha",
+                            "lazy",
+                        },
+                    },
+                    always_divide_middle = true,
+                    globalstatus = true,
+                },
+                sections = {
+                    lualine_a = { { "fancy_mode", width = 3 } },
+                    lualine_b = { components.git_repo, "branch" },
 
-					lualine_c = {
-						components.diagnostics,
-						"filename",
-					},
-					lualine_x = {
-						components.lsp_client,
-						components.spaces,
-						"encoding",
-					},
-					lualine_y = {},
-					lualine_z = { "location" },
-				},
-				inactive_sections = {
-					lualine_a = {},
-					lualine_b = {},
-					lualine_c = { "filename" },
-					lualine_x = { "location" },
-					lualine_y = {},
-					lualine_z = {},
-				},
-				extensions = { "nvim-tree", "toggleterm", "quickfix" },
-			})
-		end,
-	},
+                    lualine_c = {
+                        "filename",
+                        { "fancy_cwd" },
+                        components.diff,
+                        { "fancy_diagnostics" }, },
+                    lualine_x = {
+                        components.lsp_client,
+                        components.spaces,
+                        "encoding",
+                    },
+                    lualine_y = {},
+                    lualine_z = { "location" },
+                },
+                inactive_sections = {
+                    lualine_a = {},
+                    lualine_b = {},
+                    -- lualine_c = { "filename", path = 2 },
+                    lualine_x = { "location" },
+                    lualine_y = {},
+                    lualine_z = {},
+                },
+                extensions = { "nvim-tree", "toggleterm", "quickfix" },
+            })
+        end,
+    },
 }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -17,7 +17,11 @@ end)()
 return {
     {
         "nvim-treesitter/nvim-treesitter",
-        dependencies = { "nvim-treesitter/nvim-treesitter-textobjects", "JoosepAlviste/nvim-ts-context-commentstring" },
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+            "JoosepAlviste/nvim-ts-context-commentstring",
+            "nvim-treesitter/nvim-treesitter-context"
+        },
         build = ":TSUpdate",
         event = "BufReadPost",
         opts = {
