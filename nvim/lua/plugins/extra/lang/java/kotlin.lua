c73e5c218fca64216f85f2b28e6e01696d440e69
Josh Fried <joshfri@amazon.com>
2026-04-15 13:16:01 -0400

    chore: removing unused kotlin setup file

--- nvim/lua/plugins/extra/lang/java/kotlin.lua
+++
@@ -1,6 +0,0 @@
-return {
-    cmd = { "kotlin-language-server" },
-    single_file_support = true,
-    filetypes = { "kotlin" },
-    root_markers = { "build.gradle", "build.gradle.kts", "pom.xml" },
-}
